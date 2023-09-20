ee6a310a5aa846c04524a777efe1090bc9eb2221
zdw31-hd <145539001+zdw31-hd@users.noreply.github.com>
2023-09-20 18:01:36 +0000

    Added Entity and Math class to App

--- java-project/src/App.java
+++ java-project/src/App.java
@@ -1,5 +1,14 @@
+import eu.zdw31.hws.src.Entity;
+import eu.zdw31.hws.src.Math;
+
 public class App {
+
+    Entity e = new Entity();
+    Math m = new Math();
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // assert 1 == 2 : "Behauptung falsch";
     }
 }
